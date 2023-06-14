2c5db02affdf492dbb5a95b1c2208089635c7c99
rtxhoney <Kaushikh9831@gmail.com>
2023-06-14 19:21:12 +0530

    new push

--- anime.js
+++ anime.js
@@ -1,42 +1,52 @@
 // YouTube Data API key
 const apiKey = "AIzaSyBExZ6bdiUfjYAIHc4Ginbt9itZ_F_yR1A";
 
-// Fetch YouTube public videos
-function fetchVideos() {
-  const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&key=${apiKey}`;
+// Fetch YouTube videos based on search query
+function searchVideos() {
+  const query = document.getElementById("searchInput").value.trim();
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.items.length > 0) {
-        const videos = data.items;
+  if (query !== "") {
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${query}&key=${apiKey}`;
 
-        videos.forEach((video) => {
-          const videoId = video.id;
-          const videoTitle = video.snippet.title;
-          const videoThumbnail = video.snippet.thumbnails.medium.url;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.items.length > 0) {
+          const videos = data.items;
 
-          // Create video card
-          const cardHtml = `
-            <div class="col-md-3">
-              <div class="card">
-                <img src="${videoThumbnail}" class="card-img-top" alt="${videoTitle}">
-                <div class="card-body">
-                  <h5 class="card-title">${videoTitle}</h5>
-                  <button class="btn btn-primary" onclick="watchVideo('${videoId}')">Watch Video</button>
+          // Clear existing video container
+          document.getElementById("videoContainer").innerHTML = "";
+
+          videos.forEach((video) => {
+            const videoId = video.id.videoId;
+            const videoTitle = video.snippet.title;
+            const videoThumbnail = video.snippet.thumbnails.medium.url;
+
+            // Create video card
+            const cardHtml = `
+              <div class="col-md-3">
+                <div class="card">
+                  <img src="${videoThumbnail}" class="card-img-top" alt="${videoTitle}">
+                  <div class="card-body">
+                    <h5 class="card-title">${videoTitle}</h5>
+                    <button class="btn btn-primary" onclick="watchVideo('${videoId}')">Watch Video</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          `;
+            `;
 
-          // Append video card to the container
-          document.getElementById("videoContainer").innerHTML += cardHtml;
-        });
-      }
-    })
-    .catch((error) => {
-      console.log("Error fetching videos:", error);
-    });
+            // Append video card to the container
+            document.getElementById("videoContainer").innerHTML += cardHtml;
+          });
+        } else {
+          // No videos found for the search query
+          document.getElementById("videoContainer").innerHTML = "No videos found.";
+        }
+      })
+      .catch((error) => {
+        console.log("Error fetching videos:", error);
+      });
+  }
 }
 
 // Function to watch video
@@ -50,8 +60,3 @@ function watchVideo(videoId) {
     </div>
   `;
 }
-
-// Fetch videos on page load
-document.addEventListener("DOMContentLoaded", function () {
-  fetchVideos();
-});
